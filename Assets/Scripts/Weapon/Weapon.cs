b4c61bbbacb1fa552ce853587609fb3036442ba8
Andrea Peretti <and.xet@gmail.com>
2018-06-24 18:33:41 +0200

    Changed name from IWeapon to Weapon

---
+++ Assets/Scripts/Weapon/Weapon.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using EvilCubes.Core;
+using EvilCubes.Util;
+
+namespace EvilCubes.Weapon
+{
+    public abstract class Weapon : MonoBehaviour
+    {
+        [SerializeField]
+        protected int mPoolDimension;
+        [SerializeField]
+        protected GameObject mBulletPrefab;
+
+        protected InputManager mInputManager;
+        protected ObjectPool mBulletPool;
+
+        /////////////////////////////////////////////
+        protected void Start()
+        {
+            mInputManager = GameManager.GetInstance().GetInputManager();
+        }
+
+        /////////////////////////////////////////////
+        protected void Awake()
+        {
+            if (mBulletPrefab == null)
+            {
+                Debug.LogError("Weapon: This weapon is not correctly initialized.");
+                enabled = false;
+                return;
+            }
+            mBulletPool = new ObjectPool(mPoolDimension, mBulletPrefab);
+        }
+
+        /////////////////////////////////////////////
+        public abstract void Shoot();
+    }
+}
