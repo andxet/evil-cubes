c915605ae18d3d4d190daa993c9f82393468b889
Andrea Peretti <and.xet@gmail.com>
2018-06-27 01:14:18 +0200

    Added a utility to rotate an object randomly

---
+++ Assets/Scripts/Appearance/RotateRandomly.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace EvilCubes.Appearance
+{
+    public class RotateRandomly : MonoBehaviour
+    {
+        private Vector3 mRotationPerSeconds;
+
+        private void OnEnable()
+        {
+            mRotationPerSeconds = new Vector3(Random.Range(360, 720), Random.Range(360, 720), Random.Range(360, 720));
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            transform.Rotate(mRotationPerSeconds * Time.deltaTime);
+        }
+    }
+}
