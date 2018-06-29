0004aad38bf687167408715b048d76814d9e1c28
Andrea Peretti <and.xet@gmail.com>
2018-06-29 20:22:54 +0200

    Added a demo script

---
+++ Assets/Scripts/Demo/DemoCamera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using EvilCubes.Core;
+
+namespace EvilCubes.Demo
+{
+    public class DemoCamera : PlayerCamera
+    {
+        [SerializeField]
+        float mVelocity = 10;
+
+        // Update is called once per frame
+        void Update()
+        {
+
+            transform.localRotation *= Quaternion.AngleAxis(mVelocity * Time.deltaTime, Vector3.up);
+        }
+    }
+}
---
+++ Assets/Scripts/Demo/DemoInputManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using EvilCubes.Core;
+
+namespace EvilCubes.Demo
+{
+    public class DemoInputManager : InputManager
+    {
+        /////////////////////////////////////////////
+        void Update()
+        {
+            //Calculate the current states
+            currentMouseState[MouseState.X_DELTA] = 0.5f;
+            currentMouseState[MouseState.Y_DELTA] = 0.0f;
+
+            currentCommandState[Command.NEXT_WEAPON] = false;
+            currentCommandState[Command.PREVIOUS_WEAPON] = false;
+            currentCommandState[Command.SHOOT_DOWN] = true;
+            currentCommandState[Command.SHOOT] = true;
+            currentCommandState[Command.PAUSE] = false;
+            currentCommandState[Command.CHANGE_CAMERA] = false;
+        }
+
+        /////////////////////////////////////////////
+        new public float GetMouseState(MouseState state)
+        {
+            return currentMouseState[state];
+        }
+
+        /////////////////////////////////////////////
+        new public bool GetCommandState(Command state)
+        {
+            return currentCommandState[state];
+        }
+    }
+}
