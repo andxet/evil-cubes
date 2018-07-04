917be0f740da1a96a73735c49df3f0017557e787
Andrea Peretti <and.xet@gmail.com>
2018-07-04 22:04:39 +0200

    Fixed spawn height

--- Assets/Scripts/Core/EnemySpawnerAroundCirconference.cs
+++ Assets/Scripts/Core/EnemySpawnerAroundCirconference.cs
@@ -32,24 +32,31 @@ namespace EvilCubes.Enemy
                 Object.Destroy(enemyGameObject);
                 return null;
             }
-            enemy.transform.position = GetPosition();
+
+            //Set position
+            Vector3 position = Vector3.zero;
+            GetPosition(ref position);
+            position.y = enemy.GetHeight() / 2;
+            enemy.transform.position = position;
+
+            //Set rotation
             Vector3 lookatPosition = mPlayerObject.transform.position;
-            lookatPosition.y = enemy.GetHeight();
+            lookatPosition.y = enemy.transform.position.y;
             enemy.transform.LookAt(mPlayerObject.transform.position);
             return enemy;
         }
 
         /////////////////////////////////////////////
-        Vector3 GetPosition()
+        void GetPosition(ref Vector3 position)
         {
             //casual angle, get sin and cos
-            Vector3 playerPosition = mPlayerObject.transform.position;
             float angle = Random.Range(0, 2 * Mathf.PI);
-            Vector3 randomPosition = new Vector3(Mathf.Cos(angle), 0, Mathf.Sin(angle));
-            randomPosition.x += playerPosition.x;
-            randomPosition.z += playerPosition.z;
-            randomPosition *= DISTANCE;
-            return randomPosition;
+            Vector2 randomPosition = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle));
+            //Sum the player position, so that the player will be in the center of the circle
+            Vector3 playerPosition = mPlayerObject.transform.position;
+            position.x = randomPosition.x + playerPosition.x;
+            position.z = randomPosition.y + playerPosition.z;
+            position *= DISTANCE;
         }
     }
 }
